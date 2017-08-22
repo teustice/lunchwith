aa0b53108103f991740ce2c54910a4f3cdc945ff
Tanner Eustice <teustice@msn.com>
2017-08-22 15:25:03 -0700

    create userSeed

---
+++ src/lib/seeds/userSeed.js
@@ -0,0 +1,184 @@
+const users = [
+  {
+    id: 1,
+    name: 'Dave the Dev1',
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'Planet Argon'
+  },
+  {
+    id: 2,
+    name: 'Dave the Dev2',
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'EyeCue Lab'
+  },
+  {
+    name: 'Dave the Dev3',
+    id: 3,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'Sq1'
+  },
+  {
+    name: 'Dave the Dev4',
+    id: 4,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'New Relic'
+  },
+  {
+    name: 'Dave the Dev5',
+    id: 5,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'New Relic'
+  },
+  {
+    name: 'Dave the Dev6',
+    id: 6,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'EyeCue Lab'
+  },
+  {
+    name: 'Dave the Dev7',
+    id: 7,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'Planet Argon'
+  },
+  {
+    name: 'Dave the Dev8',
+    id: 8,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'Planet Argon'
+  },
+  {
+    name: 'Dave the Dev9',
+    id: 9,
+    skills: [
+      {
+        name: 'skill1',
+        skillId: 1
+      },
+      {
+        name: 'skill2',
+        skillId: 2
+      },
+      {
+        name: 'skill3',
+        skillId: 3
+      }
+    ],
+    bio: "Hey I'm Dave, the coolest developer of them all!",
+    company: 'New Relic'
+  }
+]
+
+export default markers;
