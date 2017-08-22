7b7bb9b755c94dd9d9f9739d8ae1e0937302aefc
Grady Shelton <gradyish@gmail.com>
2017-08-22 16:42:30 -0700

    seed profile data for a user

---
+++ src/lib/seeds/profileData.js
@@ -0,0 +1,15 @@
+const profileData = [
+  {
+    id: 1,
+    name: 'Audrey Dovetail',
+    jobTitle: 'React Native Developer',
+    employer: 'EyeCue Lab',
+    skills: {
+      'react-native',
+      'java',
+      'RESTful APIs',
+      'AWS',
+    },
+]
+
+export default profileData
