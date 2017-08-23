dc3ca57c3daa1353e4c6a2f37cb541dedae568e3
Grady Shelton <gradyish@gmail.com>
2017-08-23 11:37:29 -0700

    add saga watcher for fetch user actions

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,11 +2,13 @@ import { fork, all } from 'redux-saga/effects';
 
 import watchFetchTitle from './fetch-title';
 import watchFetchMarkers from './fetch-markers';
+import watchFetchUser from './fetch-user';
 
 
 export default function* rootSaga() {
   yield all([
     fork(watchFetchTitle),
     fork(watchFetchMarkers),
+    fork(watchFetchUser),
   ]);
 }
