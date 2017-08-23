cc70f0ec7fef5ddfe2e45d04b8bf3aed7f193b7f
Grady Shelton <gradyish@gmail.com>
2017-08-23 13:14:48 -0700

    add redux-form as a reducer to give to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,11 @@ import title from './title';
 import connectivity from './connectivity';
 import region from './region';
 import markers from './markers';
+import user from './user';
+// redux-form reducer
+import { reducer as formReducer } from 'redux-form'
 
-export const reducers = { title, connectivity, region, markers };
+export const reducers = { title, connectivity, region, markers, user, form: formReducer  };
 
 export default function getRootReducer(navReducer) {
   return combineReducers({
