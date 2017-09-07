eba69b5105aa6a78a2991c8c9d4cec73b08d0890
Grady Shelton <gradyish@gmail.com>
2017-09-07 16:58:09 -0700

    style slider component

--- src/components/Forms/slider.js
+++ src/components/Forms/slider.js
@@ -7,12 +7,13 @@ var {
   Text,
   StyleSheet,
   View,
+  Image,
 } = ReactNative;
 
 class ExperienceSlider extends React.Component {
 
   changeSliderValue(sliderValue){
-    let value = sliderValue.toFixed(1);
+    let value = sliderValue.toFixed(0);
     if(this.props.experienceSlider != value){
       this.props.setExperienceSlider(value);
       if(this.props.experienceSlider === 10){}
@@ -25,12 +26,15 @@ class ExperienceSlider extends React.Component {
           You Have About <Text style={{color: 'rgb(65,152,240)', fontFamily: 'ProximaNova-Regular'}}>{this.props.experienceSlider} Years</Text> of <Text style={{color: 'rgb(65,152,240)', fontFamily: 'ProximaNova-Regular'}}>Experience</Text>
         </Text>
         <Slider
+          thumbSize={10}
           value={parseInt(this.props.experienceSlider)}
           onValueChange={(value) => this.changeSliderValue(value)}
           maximumValue={10}
-
-          step={0.5}
+          style={{marginHorizontal: 15}}
         />
+        <Text style={styles.sliderExplanation1}>Slide the scale to indicate your total experience.</Text>
+        <Text style={styles.sliderExplanation2}>This is not about any specific skills, just the time you spent in the industry.</Text>
+
       </View>
     );
   }
@@ -48,6 +52,20 @@ var styles = StyleSheet.create({
     marginBottom: 10,
     color: 'rgb(10,10,10)',
   },
+  sliderExplanation1: {
+    fontFamily: 'ProximaNova-Regular',
+    paddingTop: 15,
+    paddingLeft: 15,
+    color: 'grey',
+    fontSize: 10,
+  },
+  sliderExplanation2: {
+    fontFamily: 'ProximaNova-Regular',
+    paddingLeft: 15,
+    paddingBottom: 20,
+    color: 'grey',
+    fontSize: 10,
+  }
 });
 
 export default ExperienceSlider;
