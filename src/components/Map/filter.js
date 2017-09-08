3a5adcf4964424fe057769319672f7db0b7fc807
Grady Shelton <gradyish@gmail.com>
2017-09-08 14:02:39 -0700

    style availability picker

--- src/components/Map/filter.js
+++ src/components/Map/filter.js
@@ -25,15 +25,15 @@ export class MapFilter extends Component {
 
   state = {
     modalVisible1: false,
-    modalVisible2: false,
+    placeHolderVisible: true,
   }
 
   setModalVisible1(visible) {
     this.setState({modalVisible1: visible});
   }
 
-  setModalVisible2(visible) {
-    this.setState({modalVisible2: visible});
+  setPlaceHolderVisible(visible) {
+    this.setState({placeHolderVisible: visible})
   }
 
   onValueChange(value: string, key: string) {
@@ -52,27 +52,39 @@ export class MapFilter extends Component {
           <Text style={staticStyles.filterText}>Devs &</Text>
           <Text style={staticStyles.filterText}>Designers</Text>
 
+          <View style={{position:'absolute', paddingTop: 100, paddingLeft: 20}}>
 
-            <Text style={staticStyles.filterAvailability}>Available between</Text>
+              <Text style={staticStyles.filterAvailability}>Available between</Text>
 
-            <TouchableWithoutFeedback
-              onPress={() => {
-                this.setModalVisible1(true)
-              }}
-            ><View><Text  style={staticStyles.time1}>11:00am and 3:00pm</Text></View></TouchableWithoutFeedback>
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  this.setModalVisible1(true)
+                  this.setPlaceHolderVisible(false)
+                }}
+              >
+                <View
+                  visible={this.state.placeHolderVisible}
+                  style={{position:'absolute', paddingTop: 100, paddingLeft: 125}}>
+                  { this.state.placeHolderVisible &&
+                    <Text  style={staticStyles.time1}>10:00am and 3:00pm</Text>
+                  }
+                </View>
+              </TouchableWithoutFeedback>
+
+          </View>
 
 
 
         </View>
         <Modal
-        animationType={"slide"}
+        animationType={"none"}
         transparent={true}
         visible={this.state.modalVisible1}
         style={staticStyles.mapBlur}>
           <Picker
           selectedValue={this.state.time}
           style={staticStyles.picker1}
-          itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'grey'}}
+          itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
           onValueChange={(itemValue, itemIndex) => this.setState({time: itemValue})}>
             <Picker.Item label="10:00am" value='0' style={{color: '#0000ff'}} />
             <Picker.Item label="10:30am" value='1' style={{color: '#0000ff'}}/>
@@ -89,7 +101,7 @@ export class MapFilter extends Component {
           <Picker
           selectedValue={this.state.time}
           style={staticStyles.picker2}
-          itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'grey'}}
+          itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
           onValueChange={(itemValue, itemIndex) => this.setState({time: itemValue})}>
             <Picker.Item label="10:30am" value='1' style={{color: '#0000ff', height: 10}}/>
             <Picker.Item label="11:00am" value='2' style={{color: '#0000ff'}}/>
@@ -103,13 +115,6 @@ export class MapFilter extends Component {
             <Picker.Item label="3:00pm" value='10' style={{color: '#0000ff'}}/>
             <Picker.Item label="3:30am" value='0' style={{color: '#0000ff'}} />
           </Picker>
-        </Modal>
-
-        <Modal
-        animationType={"slide"}
-        transparent={true}
-        visible={this.state.modalVisible2}
-        style={staticStyles.mapBlur}>
         </Modal>
       </LinearGradient>
 
@@ -148,17 +153,19 @@ const staticStyles = StyleSheet.create({
     backgroundColor: 'rgba(255,255,255,0.2)',
   },
   picker1: {
-    width: 100,
-    marginLeft: 80,
+    width: 65,
+    marginLeft: 116,
+    marginTop: -1
   },
   picker2: {
-    width: 80,
-    marginLeft: 250,
-    marginTop: -217,
+    width: 65,
+    marginLeft: 185,
+    marginTop: -216,
   },
   time1: {
+    width: 120,
     fontSize: 12,
-    color: 'grey',
+    color: 'rgb(65,152,240)',
     fontFamily: 'ProximaNova-Regular',
   },
 });
