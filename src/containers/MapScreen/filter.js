ac0433d76f1d5b7b022ca9d6d69df64d182e395c
Grady Shelton <gradyish@gmail.com>
2017-09-11 12:50:53 -0700

    close modal upon press outside of the picker

--- src/containers/MapScreen/filter.js
+++ src/containers/MapScreen/filter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, Picker, Text, TouchableHighlight, TouchableWithoutFeedback, View, StyleSheet, Dimensions, Image } from 'react-native';
+import { Modal, Picker, Text, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, View, StyleSheet, Dimensions, Image } from 'react-native';
 
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -7,12 +7,6 @@ export class MapFilter extends Component {
   constructor(props) {
     super(props);
   }
-
-  // getInitialState() {
-  //   return {
-  //     time: [0,1,2,3,4,5,6,7,8,9,10],
-  //   };
-  // }
 
   state = {
     modalVisible1: false,
@@ -28,23 +22,16 @@ export class MapFilter extends Component {
     this.setState({placeHolderVisible: visible})
   }
 
-  // onValueChange(value: string, key: string) {
-  //   const newState = {};
-  //   newState[key] = value;
-  //   this.setState(newState);
-  // }
-
-
   render() {
     return (
 
       <LinearGradient colors={['white','white','rgba(255,255,255,0)']} style={staticStyles.linearGradient}>
-      <View style={staticStyles.filterBox}>
+          <View style={staticStyles.filterBox}>
 
-          <Text style={staticStyles.filterText}>Devs &</Text>
-          <Text style={staticStyles.filterText}>Designers</Text>
+            <Text style={staticStyles.filterText}>Devs &</Text>
+            <Text style={staticStyles.filterText}>Designers</Text>
 
-          <View style={{position:'absolute', paddingTop: 100, paddingLeft: 20}}>
+            <View style={{position:'absolute', paddingTop: 100, paddingLeft: 20}}>
 
               <Text style={staticStyles.filterAvailability}>Available between</Text>
 
@@ -58,43 +45,50 @@ export class MapFilter extends Component {
                   visible={this.state.placeHolderVisible}
                   style={{position:'absolute', paddingTop: 100, paddingLeft: 125}}>
                   { this.state.placeHolderVisible &&
-                    <Text  style={staticStyles.time1}>10:00am and 3:00pm</Text>
+                    <Text  style={staticStyles.time1}>10:00am and 2:00pm</Text>
                   }
                 </View>
               </TouchableWithoutFeedback>
 
+            </View>
+
           </View>
-
-
-
-        </View>
         <Modal
         animationType={"none"}
         transparent={true}
         visible={this.state.modalVisible1}
         style={staticStyles.mapBlur}>
-          <Picker
-            selectedValue={this.props.availabilityFilter.timeStart}
-            style={staticStyles.picker1}
-            itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
-            onValueChange={(itemValue, itemIndex) => this.props.setAvailabilityFilter({ ...this.props.availabilityFilter, timeStart: itemValue})}
+          <TouchableWithoutFeedback
+            onPress={() => {
+              this.setModalVisible1(false)
+              this.setPlaceHolderVisible(true)
+            }}
           >
-            <Picker.Item label="10:00am" value='10' style={{color: '#0000ff'}} />
-            <Picker.Item label="11:00am" value='11' style={{color: '#0000ff'}}/>
-            <Picker.Item label="12:00pm" value='12' style={{color: '#0000ff'}}/>
-            <Picker.Item label="1:00pm" value='1' style={{color: '#0000ff'}}/>
-          </Picker>
-          <Picker
-            selectedValue={this.props.availabilityFilter.timeEnd}
-            style={staticStyles.picker2}
-            itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
-            onValueChange={(itemValue, itemIndex) => this.props.setAvailabilityFilter({ ...this.props.availabilityFilter, timeEnd: itemValue})}
-          >
-            <Picker.Item label="11:00am" value='11' style={{color: '#0000ff'}}/>
-            <Picker.Item label="12:00pm" value='12' style={{color: '#0000ff'}}/>
-            <Picker.Item label="1:00pm" value='1' style={{color: '#0000ff'}}/>
-            <Picker.Item label="2:00pm" value='2' style={{color: '#0000ff'}}/>
-          </Picker>
+            <View>
+              <Picker
+                selectedValue={this.props.availabilityFilter.timeStart}
+                style={staticStyles.picker1}
+                itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
+                onValueChange={(itemValue, itemIndex) => this.props.setAvailabilityFilter({ ...this.props.availabilityFilter, timeStart: itemValue})}
+              >
+                <Picker.Item label="10:00am" value='10' style={{color: '#0000ff'}} />
+                <Picker.Item label="11:00am" value='11' style={{color: '#0000ff'}}/>
+                <Picker.Item label="12:00pm" value='12' style={{color: '#0000ff'}}/>
+                <Picker.Item label="1:00pm" value='1' style={{color: '#0000ff'}}/>
+              </Picker>
+              <Picker
+                selectedValue={this.props.availabilityFilter.timeEnd}
+                style={staticStyles.picker2}
+                itemStyle={{fontSize: 12, fontFamily: 'ProximaNova-Regular', color: 'rgb(65,152,240)'}}
+                onValueChange={(itemValue, itemIndex) => this.props.setAvailabilityFilter({ ...this.props.availabilityFilter, timeEnd: itemValue})}
+              >
+                <Picker.Item label="11:00am" value='11' style={{color: '#0000ff'}}/>
+                <Picker.Item label="12:00pm" value='12' style={{color: '#0000ff'}}/>
+                <Picker.Item label="1:00pm" value='1' style={{color: '#0000ff'}}/>
+                <Picker.Item label="2:00pm" value='2' style={{color: '#0000ff'}}/>
+              </Picker>
+            </View>
+          </TouchableWithoutFeedback>
         </Modal>
       </LinearGradient>
 
@@ -105,12 +99,9 @@ export class MapFilter extends Component {
 
 const staticStyles = StyleSheet.create({
   linearGradient:{
-    // flex: 1,
     width: '100%',
     height: 175,
     position: 'absolute',
-
-
   },
   filterBox:{
     paddingTop: 25,
